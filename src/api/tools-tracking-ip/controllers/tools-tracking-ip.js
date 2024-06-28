abb076dc5eeca09d04eb86e24f8c6308d486417f
kienbui195 <kaybi9x@gmail.com>
2024-06-28 14:26:18 +0700

    update logic

--- src/api/tools-tracking-ip/controllers/tools-tracking-ip.js
+++ src/api/tools-tracking-ip/controllers/tools-tracking-ip.js
@@ -10,9 +10,8 @@ module.exports = createCoreController(
   ({ strapi }) => ({
     handleTracingIPAddress: async (ctx) => {
       try {
-        const date = ctx.request.body.data;
+        // @ts-ignore
         const ip = ctx.headers["x-forwarded-for"] || ctx.socket.remoteAddress;
-        let res;
 
         const existIp = await strapi.entityService.findMany(
           "api::tools-tracking-ip.tools-tracking-ip",
@@ -24,29 +23,31 @@ module.exports = createCoreController(
         );
 
         if (existIp.length > 0) {
-          res = await strapi.entityService.update(
+          await strapi.entityService.update(
             "api::tools-tracking-ip.tools-tracking-ip",
             existIp[0].id,
             {
               data: {
                 views: existIp[0].views + 1,
-                history: existIp[0].history + `\n[${date}]: ${ctx.request.header["user-agent"]}`,
+                history:
+                  existIp[0].history +
+                  `\n[${new Date()}]: ${ctx.request.header["user-agent"]}`,
               },
             }
           );
         } else {
-          res = await strapi.entityService.create(
+          await strapi.entityService.create(
             "api::tools-tracking-ip.tools-tracking-ip",
             {
               data: {
                 ip_address: `${ip}`,
                 views: 1,
-                history: `[${date}]: ${ctx.request.header["user-agent"]}`,
+                history: `[${new Date()}]: ${ctx.request.header["user-agent"]}`,
               },
             }
           );
         }
-        ctx.send('ok');
+        return ctx.send("ok");
       } catch (error) {
         ctx.send(
           {
